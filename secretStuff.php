234299f46745c2d79f4872d3323cae36932be4d4
Ben Redden <benjamminredden@gmail.com>
2016-01-11 08:46:04 -0500

    wonder what will happen, weird git rebasing stuff

---
+++ secretStuff.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * secret stuff, don't look =P
+ */
+$infusionsoft = new \Infusionsoft\Infusionsoft(array(
+    'clientId'     => 'zjdmn6jek8xd5v6yz9eptc8m',
+    'clientSecret' => 'ReFbmkmPWq',
+    'redirectUri'  =>  $redirectUrl
+));
